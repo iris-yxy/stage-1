ff1420879b82d118efc94fe9e3deb130165cac0f
iris-yxy <z5515011@ad.unsw.edu.au>
2025-08-11 15:19:45 +0800

    Step 2: Add tab buttons and useState to track active tab

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import './APP.css';
-function App(){
+import React,{useState}from 'react';
+const data = {
+  Fruits: ['Apple', 'Banana', 'Pear', 'Watermelon', 'Grape', 'Strawberry', 'Mango', 'Blackberry'],
+  Spices: ['Salt', 'Pepper', 'Chilli', 'Herbs', 'Curry'],
+  Vegetables: ['Carrot', 'Cucumber'],
+};
+
+function App() {
+  const tabs = Object.keys(data);
+  const [activeTab,setAcitiveTab]=useState('Fruits');
   return(
-    <div ClassName="container">
+    <div>
       <h1>Item Selector</h1>
+      <div>
+        {tabs.map((tab)=>(
+          <button
+          key={tab}
+          onClick={()=>setAcitiveTab(tab)}>{tab}
+          </button>
+        ))}
+      </div>
       </div>
   );
 }
